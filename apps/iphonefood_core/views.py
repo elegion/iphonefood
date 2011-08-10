644e193201ee68444a607d847d272ed5843b9874
Anatoly Larin <me@alarin.ru>
2011-08-10 21:52:10 +0400

    [*] non encode json to ansii

--- apps/iphonefood_core/views.py
+++ apps/iphonefood_core/views.py
@@ -9,17 +9,17 @@ from iphonefood_core.models import Dish, Address
 
 def dishes(request):
     data = Dish.objects.all().values('category__name', 'name', 'price', 'rating', 'description', 'photo_url')
-    return HttpResponse(json.dumps(list(data),sort_keys=True, indent=4), mimetype='application/json')
+    return HttpResponse(json.dumps(list(data),sort_keys=True, indent=4, ensure_ascii=False), mimetype='application/json')
 
 
 def top(request):
     data = (Dish.objects.exclude(rating=0)\
         .order_by('-rating')
         .values('category__name', 'name', 'price', 'rating', 'description', 'photo_url'))
-    return HttpResponse(json.dumps(list(data),sort_keys=True, indent=4), mimetype='application/json')
+    return HttpResponse(json.dumps(list(data),sort_keys=True, indent=4, ensure_ascii=False), mimetype='application/json')
 
 
 def addresses(request):
     data = Address.objects.all().values('city', 'district', 'address', 'description')
-    return HttpResponse(json.dumps(list(data),sort_keys=True, indent=4), mimetype='application/json')
+    return HttpResponse(json.dumps(list(data),sort_keys=True, indent=4, ensure_ascii=False), mimetype='application/json')
 
